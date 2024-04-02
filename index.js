c01bc1b7e8d73b2a0b22c69f81bc24ff065253f7
Dagmara Wąchocka <dwachocka@gmail.com>
2024-04-02 13:07:54 +0200

    change html name, corrections in js and styles.css

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-//////// FIRST DICE ////////
+function rollDice() {
+  const randomNumber1 = Math.ceil(Math.random() * 6);
+  document.querySelector("img.img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-var randomNumber1 = Math.ceil(Math.random() * 6);
-document.querySelector("img.img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  const randomNumber2 = Math.ceil(Math.random() * 6);
+  document.querySelector("img.img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-//////// SECOND DICE ////////
-
-var randomNumber2 = Math.ceil(Math.random() * 6);
-document.querySelector("img.img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-
-
-//////// PĘTLA WHO WON / DRAW ////////
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("div h1").textContent = "🏆 Player 1 Won";
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("div h1").textContent = "Player 2 Won 🏆";
-} else if (randomNumber1 === randomNumber2) {
-  document.querySelector("div h1").textContent = "It is a DRAW!";
+  if (randomNumber1 > randomNumber2) {
+    return document.querySelector("div h1").textContent = "🏆 Player 1 Won";
+  } else if (randomNumber2 > randomNumber1) {
+    return document.querySelector("div h1").textContent = "Player 2 Won 🏆";
+  } else if (randomNumber1 === randomNumber2) {
+    return document.querySelector("div h1").textContent = "It is a DRAW!";
+  }
 }
+
+const rollButton = document.querySelector(".roll-button");
+rollButton.addEventListener('click', rollDice);
+
